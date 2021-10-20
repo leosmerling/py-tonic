0fb4bc5616bdcf5de0c366c145a20b71aa3d48a1
Leo Smerling <leosmerling@noreply.github.com>
2021-10-20 11:58:53 +0200

    Making proto objects usable in python

--- build.rs
+++ build.rs
@@ -1,7 +1,10 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
+        .out_dir("proto/")
         .build_server(false)
-        .type_attribute(".", "#[::pyo3_prost::pyclass_for_prost_struct]")
+        .build_client(false)
+        //.type_attribute(".", "#[::pyo3::prelude::pyclass]")
+        //.type_attribute(".", "#[::pyo3_prost::pyclass_for_prost_struct]")
         .compile(&["proto/hw.proto"], &["proto/"])?;
     Ok(())
 }
--- python/greeter/say_hello.py
+++ python/greeter/say_hello.py
@@ -1,7 +1,9 @@
-from greeter.hw import HelloResponse
+from greeter.hw import HelloRequest, HelloResponse
 
 def execute(request):
-    print("PYTHON", request, type(request))
+    print("PYTHON", request, type(request), type(request).__dict__)
+    # print("encode", request.encode())
+    # print("decode", HelloRequest.decode(request.encode()))
     response = HelloResponse()
     response.message = f"Hello {request.name}"
     print("PYTHON", response, type(response))
