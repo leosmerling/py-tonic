8796905b1f1594924381e9effe40afc447c5053a
Leo Smerling <leosmerling@noreply.github.com>
2021-10-20 14:15:41 +0200

    Comparison with python proto generated code

--- python/compile-proto.sh
+++ python/compile-proto.sh
@@ -1,3 +1,1 @@
-mkdir hw
-touch hw/__init.py
-python -m grpc_tools.protoc -I=../proto --python_out=./hw ../proto/hw.proto
+python -m grpc_tools.protoc -I=../proto --python_out=./greeter ../proto/hw.proto
--- python/greeter/say_hello.py
+++ python/greeter/say_hello.py
@@ -1,10 +1,26 @@
-from greeter.hw import HelloRequest, HelloResponse
+import gc
+import os
+import psutil
+
+from greeter.hw import Extra, HelloRequest, HelloResponse
 
 def execute(request):
-    print("PYTHON", request, type(request), type(request).__dict__)
+    # print("PYTHON", request, type(request), type(request).__dict__)
     # print("encode", request.encode())
     # print("decode", HelloRequest.decode(request.encode()))
     response = HelloResponse()
     response.message = f"Hello {request.name}"
-    print("PYTHON", response, type(response))
+    # response.extra = request.extra
+    extra = Extra()
+    extra.text= request.extra.text + " processed"
+    extra.num = request.extra.num + 1
+    response.extra = extra
+    # print("PYTHON", response, type(response))
     return response
+
+
+def memory_footprint():
+    """Returns memory (in MB) being used by Python process"""
+    # gc.collect()
+    mem = psutil.Process(os.getpid()).memory_info().rss
+    return int(mem / 1024 )  # snapshot
